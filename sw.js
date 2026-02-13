2bab90882d2f072af4f67f9fde746ef5663d059b
heptodrift <prendiville@gmail.com>
2026-02-13 18:03:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'chief-log-v1';
+
+const CACHE_NAME = 'chief-log-v1.1'; // Incremented to force update
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json',
+    './',
+    './index.html',
+    './manifest.json',
     'https://cdn.tailwindcss.com',
     'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&family=Inter:wght@400;600;800&display=swap'
 ];
@@ -11,6 +12,20 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
